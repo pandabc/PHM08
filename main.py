8b703032299b43ab512be287d449620858a930c4
wanglinhan <wanglinhan@ablecloud.cn>
2017-02-17 14:16:59 +0800

    add PHM08.py

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
-import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import matplotlib.pyplot as plt
 
-x = np.linspace(-2*np.pi, 2*np.pi, 100)
-y = np.sin(x)/x
 
-plt.plot(x, y)
+filename = './data/train_FD002.txt'
+
+unit = []
+x = []
+y = []
+z = []
+
+with open(filename, 'r') as f:
+    for line in f:
+        cols = line.strip().split()
+        unit.append(int(cols[0]))
+        x.append(float(cols[2]))
+        y.append(float(cols[3]))
+        z.append(float(cols[4]))
+
+unit = np.array(unit)
+x = np.array(x)
+y = np.array(y)
+z = np.array(z)
+
+idx = np.where(unit > 0)
+
+x = x[idx]
+y = y[idx]
+z = z[idx]
+
+print len(z)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+ax.scatter(x, y, z)
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
 plt.show()
+
